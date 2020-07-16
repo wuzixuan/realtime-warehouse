ed0985a44aa1d8aeaafc8c83c285ada8c4e81538
zixuan <x950913@outlook.com>
2020-07-16 23:45:44 +0800

    1

--- src/main/scala/com/zixuan/actions/KafkaToKudu.scala
+++ src/main/scala/com/zixuan/actions/KafkaToKudu.scala
@@ -13,6 +13,8 @@ class KafkaToKudu {
     env.getCheckpointConfig.setCheckpointingMode(CheckpointingMode.EXACTLY_ONCE)
     // 确保检查点之间有最小间隔为500 ms，假设每10s做一次checkpoint，某次耗时9s，那么正常在本次checkpoint完成后的1s又该做checkpoint了，以下配置可以确保每次checkpoint的最小间隔。
     env.getCheckpointConfig.setMinPauseBetweenCheckpoints(5000)
+    //设置状态后端为rockDB
+
     val flinkKafkaUtil = new FlinkKafkaUtil
     val sourceDStream = flinkKafkaUtil.getSource(env, "test")
 
