53c3232ec5a3f2fab04cc95198d156f06b8725f3
zixuan <x950913@outlook.com>
2020-08-17 00:00:37 +0800

    1

--- src/main/java/com/zixuan/kafka/encoder/FlinkKafkaObjectDeserialization.java
+++ src/main/java/com/zixuan/kafka/encoder/FlinkKafkaObjectDeserialization.java
@@ -2,6 +2,7 @@ package com.zixuan.kafka.encoder;
 
 import com.zixuan.kafka.utils.BeanUtils;
 import org.apache.flink.api.common.serialization.DeserializationSchema;
+import org.apache.flink.api.common.typeinfo.TypeHint;
 import org.apache.flink.api.common.typeinfo.TypeInformation;
 
 import java.io.IOException;
@@ -11,11 +12,13 @@ public class FlinkKafkaObjectDeserialization implements DeserializationSchema<Ob
         return BeanUtils.BytesToObject(bytes);
     }
 
+    //是否是最后一条数据，因为使用的是无界流而非批处理，所以不存在最后一条数据，直接返回false
     public boolean isEndOfStream(Object o) {
         return false;
     }
 
+    //指定数据类型
     public TypeInformation getProducedType() {
-        return null;
+       return null;
     }
 }
--- src/main/scala/com/zixuan/actions/KafkaToKafka.scala
+++ src/main/scala/com/zixuan/actions/KafkaToKafka.scala
@@ -3,8 +3,10 @@ package com.zixuan.actions
 import java.util.Properties
 
 import com.zixuan.kafka.encoder.FlinkKafkaObjectDeserialization
+import org.apache.flink.api.common.restartstrategy.RestartStrategies
 import org.apache.flink.api.java.utils.ParameterTool
 import org.apache.flink.contrib.streaming.state.RocksDBStateBackend
+import org.apache.flink.runtime.executiongraph.restart.RestartStrategy
 import org.apache.flink.runtime.state.StateBackend
 import org.apache.flink.streaming.api.CheckpointingMode
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
@@ -31,8 +33,19 @@ object KafkaToKafka {
     val backend:StateBackend = new RocksDBStateBackend("hdfs://hd01:9000/user/flink/StateBackend")
     env.setStateBackend(backend)
 
+    //设置重启策略,重启3次，间隔1分钟
+    env.setRestartStrategy(RestartStrategies.fixedDelayRestart(3,60000L))
+
     //消费kafka
+    //导入隐式转换，使Buffer可以转换成java的buffer
+    //因为flink底层使java写的，消费kafka的多个topic时需要传入一个java的集合。所以需要进行转换。
+    import scala.collection.JavaConverters._
+    //调用param从TOPIC中获取topic，可传入多个topic用","隔开
+    params.get(TOPIC).split(",").toBuffer.asJava
+
+
     val properties = new Properties()
+    //获取Properties对象，在配置文件中配置好kafka集群、消费者组、以及其他参数
     val fs = this.getClass.getClassLoader.getResourceAsStream("conf/test.properties")
     properties.load(fs)
     import org.apache.flink.api.scala._
